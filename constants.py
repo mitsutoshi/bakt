44860012ba179de524fa7df085cfe3cc645bafec
kobin <mitsu3204@gmail.com>
2019-02-11 12:29:26 +0900

    定数管理ファイルを追加

---
+++ constants.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+ORDER_STATUS_ACTIVE = 'ACTIVE'  # type: str
+"""注文ステータス：アクティブ"""
+
+ORDER_STATUS_CANCELED = 'CANCELED'  # type: str
+"""注文ステータス：キャンセル済み"""
+
+ORDER_STATUS_COMPLETED = 'COMPLETED'  # type: str
+"""注文ステータス：約定済み"""
+
+ORDER_TYPE_LIMIT = 'LIMIT'  # type: str
+"""注文種別：指値"""
+
+ORDER_TYPE_MARKET = 'MARKET'  # type: str
+"""注文種別：成行"""
+
+SIDE_BUY = 'BUY'  # type: str
+"""売買種別：買い"""
+
+SIDE_SELL = 'SELL'  # type: str
+"""売買種別：売り"""
